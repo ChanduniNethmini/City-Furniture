4ce9dffe795e630d2f80990edadcc9527fcb0cf8
ChanduniNethmini <nchanduni21@gmail.com>
2021-09-12 22:21:04 +0530

    added routes and models

---
+++ models/posts.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const postSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    postalNo:{
+        type:Number,
+        required:true
+    },
+    street:{
+        type:String,
+        required:true
+    },
+    town:{
+        type:String,
+        required:true
+    },
+    contactNo:{
+        type:Number,
+        required:true
+    },
+    orderDate:{
+        type:Date,
+    },
+    status:{
+        type:String,
+        default:"Pending"
+    }
+
+     
+    
+});
+
+module.exports = mongoose.model('Posts',postSchema);
